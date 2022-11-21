b4551959447fe08db1d4c41a2999ee5ffe0df8e9
walaaalshaikh <walaalshaikh94@gmail.com>
2022-11-21 10:33:09 +0300

    creating the ui of log out screen

--- lib/view/screens/auth/log_out_screen.dart
+++ lib/view/screens/auth/log_out_screen.dart
@@ -1,13 +1,78 @@
-
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../../logic/controllers/auth_controllers.dart';
 
 class LogOutScreen extends StatelessWidget {
-  const LogOutScreen({super.key});
+  LogOutScreen({super.key});
+
+  final controller = Get.put(AuthController());
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("LogOut"),
+    return GetBuilder<AuthController>(
+      builder: (_) => Material(
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: () {
+            Get.defaultDialog(
+              title: "Logout From App",
+              titleStyle: const TextStyle(
+                fontSize: 18,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+              middleText: 'Are you sure you need to logout',
+              middleTextStyle: const TextStyle(
+                fontSize: 18,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+              backgroundColor: Colors.grey,
+              radius: 10,
+              textCancel: " No ",
+              cancelTextColor: Colors.white,
+              textConfirm: " YES ",
+              confirmTextColor: Colors.white,
+              onCancel: () {
+                Get.back();
+              },
+              onConfirm: () {
+                controller.signOut();
+              },
+              buttonColor: Colors.black,
+            );
+          },
+          splashColor: Colors.red,
+          borderRadius: BorderRadius.circular(12),
+          customBorder: const StadiumBorder(),
+          child: Row(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(6),
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.black,
+                ),
+                child: const Icon(
+                  Icons.logout,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              const Text(
+                "Logout",
+                style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
