0120f09e49d3c0b80d4d4368524209b302fbf959
walaaalshaikh <walaalshaikh94@gmail.com>
2022-11-21 10:24:18 +0300

    implementing the function of logout

--- lib/logic/controllers/auth_controllers.dart
+++ lib/logic/controllers/auth_controllers.dart
@@ -1,7 +1,43 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthController extends GetxController{
 
+  FirebaseAuth auth = FirebaseAuth.instance;
+  var googleSignIn=GoogleSignIn();
+  var displayName="".obs;
+  var displayUserPic="".obs;
+  var isSignedIn=false;
+  final GetStorage authBox=GetStorage();
 
+
+  void signOut()async{
+    try{
+
+      await auth.signOut();
+      await googleSignIn.signOut();
+      displayName.value="";
+      displayUserPic.value='';
+      isSignedIn=false;
+      authBox.remove("auth");
+      update();
+
+
+
+
+
+    }catch(e){
+      Get.snackbar(
+          "Error!",
+          e.toString(),
+          snackPosition:SnackPosition.BOTTOM,
+          backgroundColor: Colors.grey,
+          colorText: Colors.white);
+    }
+
+  }
 
 }
