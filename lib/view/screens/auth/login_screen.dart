578c0e83e243bb7e29e9ce1c1b128a37e3ffc556
Renad alotaibi <renadayad77@gmail.com>
2022-11-21 09:34:14 +0300

    uploed project

---
+++ lib/view/screens/auth/login_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Login_Screen extends StatelessWidget {
+  const Login_Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("login"),
+    );
+  }
+}
