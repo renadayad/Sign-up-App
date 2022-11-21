188b1e3fe1ba798561cf41ae82375149e1d72ba5
walaaalshaikh <walaalshaikh94@gmail.com>
2022-11-21 09:56:41 +0300

    creating the files for the project

---
+++ lib/view/screens/auth/forgot_password_screen.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class ForgotPasswordScreen extends StatelessWidget {
+  const ForgotPasswordScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Forgot Password"),
+    );
+  }
+}
---
+++ lib/view/screens/auth/log_out_screen.dart
@@ -0,0 +1,13 @@
+
+import 'package:flutter/material.dart';
+
+class LogOutScreen extends StatelessWidget {
+  const LogOutScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("LogOut"),
+    );
+  }
+}
